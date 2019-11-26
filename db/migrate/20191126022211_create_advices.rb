a33036e1a5419f55924e596d2165f79ba06fe283
Kate Follett <kate.follett@baruchmail.cuny.edu>
2019-11-25 21:31:01 -0500

    Created advice resources

---
+++ db/migrate/20191126022211_create_advices.rb
@@ -0,0 +1,10 @@
+class CreateAdvices < ActiveRecord::Migration[5.2]
+  def change
+    create_table :advices do |t|
+      t.string :quote
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
