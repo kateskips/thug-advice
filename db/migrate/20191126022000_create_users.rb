7532d1cb719676583ca0826fcc1194e89ef55693
Kate Follett <kate.follett@baruchmail.cuny.edu>
2019-12-02 17:27:05 -0500

    added references to users table

--- db/migrate/20191126022000_create_users.rb
+++ db/migrate/20191126022000_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
+      t.references :advice, foreign_key: true
     end
   end
 end
