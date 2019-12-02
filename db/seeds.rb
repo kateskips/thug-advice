da7c5090262521d9f4448cfce8424333c6c4f6b2
Kate Follett <kate.follett@baruchmail.cuny.edu>
2019-12-02 13:25:37 -0500

    Set up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_a = User.create(name: "Vina")
+user_b = User.create(name: "Roy")
+user_c = User.create(name: "Anna")
+user_d = User.create(name: "Adam")
+
+advice_a = Advice.create(quote: "Don't stand for Bullshit.", user: user_d)
+advice_b = Advice.create(quote: "You have the choice to do anything.", user: user_c)
+advice_c = Advice.create(quote: "Be the best you can be bitch.", user: user_a)
+advice_d = Advice.create(quote: "Be humble", user: user_b)
