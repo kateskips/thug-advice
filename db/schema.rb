bda677b41d3256a1ab506901fb892ad4d1f6728d
Kate Follett <kate.follett@baruchmail.cuny.edu>
2019-12-02 16:55:36 -0500

    eliminated date times from the schema

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,5 @@ ActiveRecord::Schema.define(version: 2019_11_26_022211) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
-
 end
