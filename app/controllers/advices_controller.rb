7c7779421a156f88b1b08ae8dce7a9e4f1c4fbbe
Kate Follett <kate.follett@baruchmail.cuny.edu>
2019-12-02 21:31:46 -0500

    added controllers for advice

--- app/controllers/advices_controller.rb
+++ app/controllers/advices_controller.rb
@@ -1,2 +1,38 @@
 class AdvicesController < ApplicationController
+    def index
+    render json: Advice.all
+    end
+
+    def new
+    advice = Advice.new
+    end
+
+    def create
+    render json: Advice.new(advice_params)
+    if advice.valid?
+        advice.save
+    else
+        render json: new
+      end
+    end
+
+    def show
+    render json: Advice.find(params[:id])
+    end
+
+    def update
+    render json: advice.update(advice_params)
+    end
+
+    def destroy
+    render json: advice.destroy
+    end
+
+    private
+
+    def advice_params
+        params.require(:advice).permit(:quote, :user_id)
+    end
 end
+
+
