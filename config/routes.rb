8b2e0a5bb1405792a97e03b8a6779a63a6a73dd5
Kate Follett <kate.follett@baruchmail.cuny.edu>
2019-12-18 18:52:33 -0500

    updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :advices
+  get '/advices' => 'advices#index'
+  get '/advices/:id' => 'advices#show'
+  
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
