e1cf6041498b335708bcc9f91f32e116e80c197b
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-01-03 20:33:47 -0500

    updated advice serializer

--- app/serializers/advice_serializer.rb
+++ app/serializers/advice_serializer.rb
@@ -1,3 +1,30 @@
 class AdviceSerializer < ActiveModel::Serializer
+     def index
+    advices = Advice.all
+    render json: AdviceSerializer.new(advices).to_serialized_json
+  end
+
+   def show
+    advices = Advice.find_by(id: params[:id])
+    render json: AdviceSerializer.new(advice).to_serialized_json
+  end
+
+  #def new
+  #  advice = Advice.new
+  #  render json: AdviceSerializer.new(advice).to_serialized_json
+  #end
+
+  #def create
+   # render json: AdviceSerializer.new(advice).to_serialized_json
+   # if advice.valid?
+    #    advice.save
+  #  else
+   #     render json: new
+   #   end
+   # end
+  
+  # end
+  
+
     attributes :id, :quote, :user
 end
