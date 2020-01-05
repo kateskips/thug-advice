04e37670d8c2dbfc8178ef439d606246091a6a77
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-01-05 18:44:27 -0500

    Clean up code comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  #get '/advices' => 'advices#index'
-  #get '/advices/:id' => 'advices#show'
-  #post '/advices' => 'advices#create'
+  # get '/advices' => 'advices#index'
+  # get '/advices/:id' => 'advices#show'
+  # post '/advices' => 'advices#create'
   #put '/advices/:id' => 'advices#update'
   resources :advices
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+ 
