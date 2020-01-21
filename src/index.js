e1a4d950ed97bfeacba1bd735a461db0acc75399
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-01-20 22:35:38 -0500

    progress commenting code

--- src/index.js
+++ src/index.js
@@ -10,33 +10,45 @@ document.addEventListener('DOMContentLoaded', () => {
   let typeCollection = document.querySelector('.type-collection');
   let currentType = null;
   
+  // fetch all the advice from the backend, regardless of category.
+  // Returns a promise.
   fetch("http://localhost:3000/advices")
+    // parse the response if/when it arrives as json.
     .then(resp => resp.json())
+    // stores it in variable called arrayOfQuotes
     .then(arrayOfQuotes => {
+      // when then is transfered into saveAllAdvices, which is null(empty)
       saveAllAdvices = arrayOfQuotes;
     })
   
+  //created function called noAdviceDom 
   function noAdviceDom() {
+    //Created an instance of the element for the specified tag + created a variable noAdviceDiv
     const noAdviceDiv = document.createElement("div");
+    // creating a block with curly brackets(this limits the scope)
+   // In this case, create an element and appendchild to that element with a TEXT.  
     {
-      const c = document.createElement("h3");
-      c.appendChild(document.createTextNode("Thug cannot help you"));
+      const c = document.createElement("h1");
+      c.appendChild(document.createTextNode("Thug cannot help you."));
       noAdviceDiv.appendChild(c);
     }
     {
-      const c = document.createElement("p");
+      const c = document.createElement("h3");
       c.appendChild(document.createTextNode("Got no advice, dawg."));
       noAdviceDiv.appendChild(c);
     }
     return noAdviceDiv;
   }
-  
+
+  // created a function called adviceDom. The same as noAdviceDom but with this it takes in advice in the parameters.
   function adviceDom(advice) {
     const adviceDiv = document.createElement("div");
     adviceDiv.setAttribute("class", "advice-card");
     adviceDiv.innerHTML = `<h1>${advice.quote}</h1>`;
     return adviceDiv;
   }
+
+
   
   if (typesRow != null) {
     fetch('http://localhost:3000/users')
@@ -50,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault();
             typeCollection.innerHTML =
               `<div class="type-card">
-              <h1>${type.name_type}</h1></div>`;
+              <h1><u>${type.name_type} Advices</u></h1></div>`;
             fetch(`http://localhost:3000/advices?user_id=${type.id}`)
               .then(resp => resp.json())
               .then(arrayOfQuotes => {
