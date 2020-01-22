24ee117fb58d284fb5ac8616d541571c373fefa6
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-01-22 12:31:25 -0500

    document created the radio button

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let adviceBtn = document.querySelector("#gotchu");
-  let adviceCollection = document.querySelector(".advice-collection");
-  let submitBtn = document.querySelector('#submit');
-  let adviceForm = document.querySelector('#adviceForm');
+  const adviceBtn = document.querySelector("#gotchu");
+  const adviceCollection = document.querySelector(".advice-collection");
+  const adviceForm = document.querySelector('#adviceForm');
+  const typesRow = document.querySelector('#types');
+  const typeCollection = document.querySelector('.type-collection');
+
   let saveAdvices = null;
   let saveAllAdvices = null;
   let saveTypes = null;
-  const typesRow = document.querySelector('#types');
-  let typeCollection = document.querySelector('.type-collection');
   let currentType = null;
-  
+
   // fetch all the advice from the backend, regardless of category.
   // Returns a promise.
   fetch("http://localhost:3000/advices")
@@ -20,13 +20,13 @@ document.addEventListener('DOMContentLoaded', () => {
       // when then is transfered into saveAllAdvices, which is null(empty)
       saveAllAdvices = arrayOfQuotes;
     })
-  
+
   //created function called noAdviceDom 
   function noAdviceDom() {
     //Created an instance of the element for the specified tag + created a variable noAdviceDiv
     const noAdviceDiv = document.createElement("div");
     // creating a block with curly brackets(this limits the scope)
-   // In this case, create an element and appendchild to that element with a TEXT.  
+    // In this case, create an element and appendchild to that element with a TEXT.  
     {
       const c = document.createElement("h1");
       c.appendChild(document.createTextNode("Thug cannot help you."));
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
-  
+
   if (typesRow != null) {
     fetch('http://localhost:3000/users')
       .then(resp => resp.json())
@@ -72,13 +72,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 for (const advice of arrayOfQuotes) {
                   typeCollection.appendChild(adviceDom(advice));
                 }
-              }) 
+              })
           })
           typesRow.appendChild(b);
         }
       })
   }
-        
+
   if (adviceBtn != null) {
     adviceBtn.addEventListener('click', (e) => {
       e.preventDefault();
@@ -97,33 +97,56 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       adviceCollection.appendChild(card);
     })
-
-    if (submitBtn != null) {
-      submitBtn.addEventListener('submit', (event) => {
-        event.preventDefault();
-        fetch('http://localhost:3000/advices', {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify({
-            'quote':
-            'user_id'
-          })
-        }).then(response => response.json())
-        alert('You just submitted!')
-        .then((responseJson) => console.log(responseJson))
-      });
-    }
   }
-    
+
+  if (adviceForm != null) {
+    const submitBtn = document.querySelector('#submit');
+    console.assert(submitBtn != null, "no submit button yet do have advice");
+    console.log("asdf");
+    fetch("http://localhost:3000/users")
+      .then(resp => resp.json())
+      .then(arrayOfTypes => {
+        for (const type of arrayOfTypes) {
+          const radioLabel = document.createElement("label");
+          const radioButton = document.createElement("input");
+          const span = document.createElement("span");
+          const p = document.createElement("p");
+          radioButton.setAttribute("name", "create-advice-type");
+          radioButton.setAttribute("class", "with-gap");
+          radioButton.setAttribute("type", "radio");
+          adviceForm.appendChild(p);
+          p.appendChild(radioLabel);
+          radioLabel.appendChild(radioButton);
+          span.appendChild(document.createTextNode(type.name_type));
+          radioLabel.appendChild(span);
+          adviceForm.appendChild(radioLabel);
+        }
+      })
+    submitBtn.addEventListener('submit', (event) => {
+      event.preventDefault();
+      fetch('http://localhost:3000/advices', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          'quote':
+            'user_id'
+        })
+      }).then(response => response.json())
+        .then((responseJson) => {
+          alert('You just submitted!');
+          console.log(responseJson);
+        })
+    });
+  }
 })
 
 
 
 
-  
+
 
 
 
