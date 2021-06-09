4e26a1d711258b27b5b74d2fe42f8daa5f0c14df
Kate Follett <git@klgf.dev>
2021-06-09 00:12:51 -0400

    convert app to use PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_11_26_022211) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "advices", force: :cascade do |t|
     t.integer "user_id"
     t.string "quote"
